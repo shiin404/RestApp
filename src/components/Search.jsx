ac00b2b9cc7b67b616d218f5ff41c232aba8cc83
shiin404 <akashigameplayyt@gmail.com>
2025-12-03 13:18:20 +0600

    Adaptive 1

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,27 @@
-import styles from './Search.module.css'
-export default function Search(){
-    return(
-        <section className={styles.searchsection}>
-            <div className={styles.searchcontainer}>
-                <div className={styles.searchleft}>
-                <span className={styles.searchlabel}>Поиск</span>
-                <span className={styles.searchplaceholder} >ресторана, гостиниц, цветочных магазинов</span>
-                </div>
-                <div className={styles.searchaction}>Найти</div>
-            </div>
-        </section>
-    )
+import styles from './Search.module.css';
+
+export default function Search() {
+  return (
+    <section className={styles.searchSection}>
+      <div className={styles.searchContainer}>
+
+        {/* Кнопки выбора категории */}
+        <div className={styles.categoryButtons}>
+          <button className={styles.categoryBtn}>Ресторан</button>
+          <button className={styles.categoryBtn}>Отель</button>
+          <button className={styles.categoryBtn}>Цветочная</button>
+        </div>
+
+        {/* Поле поиска */}
+        <input
+          type="text"
+          placeholder="Введите название заведения"
+          className={styles.searchInput}
+        />
+
+        {/* Кнопка Найти */}
+        <button className={styles.searchAction}>Найти</button>
+      </div>
+    </section>
+  );
 }
