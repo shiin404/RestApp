09856dfb6523965f98c832a4e598d2b5e0bc1d3a
shiin404 <akashigameplayyt@gmail.com>
2026-01-15 16:38:46 +0600

    New footer

--- src/components/MainPage/Footer.jsx
+++ src/components/MainPage/Footer.jsx
@@ -1,16 +1,17 @@
-import styles from "./Footer.module.css"; // убедись, что файл именно так называется
-import telegram from '../../assets/telegram.png'
-import whatspp from '../../assets/whatsapp.png'
-import instagram from '../../assets/instagram.jpeg'
+import styles from "./Footer.module.css";
+import telegram from '../../assets/telegram.png';
+import whatspp from '../../assets/whatsapp.png';
+import instagram from '../../assets/instagram.jpeg';
 import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className={styles.siteFooter}>
       <div className={styles.footerContent}>
 
         <div className={styles.footerCol}>
-          <h4>RestApp</h4>
-          <p className={styles.topSlogan}>Лучшие сервисы для вашего комфорта</p>
+          <h4 className={styles.logo}>RestApp</h4>
+          <p className={styles.topSlogan}>Ваш ключ к комфорту в один клик</p>
           <div className={styles.footerSocialIcons}>
             <a href="https://wa.me/+77028872323" target="_blank" rel="noreferrer">
               <img src={whatspp} alt="WhatsApp" />
@@ -19,31 +20,35 @@ export default function Footer() {
               <img src={telegram} alt="Telegram" />
             </a>
             <a href="https://www.instagram.com/restapp.kz/" target="_blank" rel="noreferrer">
-              <img src={instagram } alt="Instagram" />
+              <img src={instagram} alt="Instagram" />
             </a>
           </div>
         </div>
 
         <div className={styles.footerCol}>
-          <h4>Навигация</h4>
-          <ul>
-            <li><a href="restaurants">Рестораны</a></li>
-            <li><a href="hotels">Гостиницы</a></li>
-            <li><a href="flowers">Цветочные магазины</a></li>
+          <h4 className={styles.colTitle}>Навигация</h4>
+          <ul className={styles.navList}>
+            <li><Link to="/restaurants">Рестораны</Link></li>
+            <li><Link to="/hotels">Гостиницы</Link></li>
+            <li><Link to="/flowers">Цветочные магазины</Link></li>
           </ul>
         </div>
 
         <div className={styles.footerCol}>
-          <h4>Приложение</h4>
+          <h4 className={styles.colTitle}>Мобильный сервис</h4>
           <Link to="/app" className={styles.downloadLink}>
-            <button className={styles.downloadApp}>Скачать RestApp</button>
+            <button className={styles.downloadApp}>
+              Скачать приложение
+              <span className={styles.btnArrow}>→</span>
+            </button>
           </Link>
         </div>
 
       </div>
 
       <div className={styles.footerBottom}>
-        ©️ 2025 RestApp. Все права защищены.
+        <div className={styles.divider}></div>
+        <p>©️ 2026 RestApp. Все права защищены.</p>
       </div>
     </footer>
   );
